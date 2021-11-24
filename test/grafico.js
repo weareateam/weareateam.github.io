1c883b6762f2b0841bb9fc87c53499a3bcaba43d
weareateam <94444452+weareateam@users.noreply.github.com>
2021-11-24 21:08:37 +0100

    Add files via upload

--- test/grafico.js
+++ test/grafico.js
@@ -1,64 +1,10 @@
-// video
-
-function lightbox_open() {
-  var lightBoxVideo = document.getElementById("VisaChipCardVideo");
-  window.scrollTo(0, 0);
-  document.getElementById('light').style.display = 'block';
-  document.getElementById('fade').style.display = 'block';
-  lightBoxVideo.play();
-}
-
-function lightbox_close() {
-  var lightBoxVideo = document.getElementById("VisaChipCardVideo");
-  document.getElementById('light').style.display = 'none';
-  document.getElementById('fade').style.display = 'none';
-  lightBoxVideo.pause();
-
-  // start timer
-  document.getElementById('timer').innerHTML =
-  01 + ":" + 00;
-  startTimer();
-
-  // play music
-  var linkAudio = 'base.m4a';
-  audioObj = new Audio(linkAudio).play();
-}
-
-// timer
-
-function startTimer() {
-  var presentTime = document.getElementById('timer').innerHTML;
-  var timeArray = presentTime.split(/[:]+/);
-  var m = timeArray[0];
-  var s = checkSecond((timeArray[1] - 1));
-  if(s==59){m=m-1}
-  if(m<0){
-    return
-  }
-  
-  document.getElementById('timer').innerHTML =
-    m + ":" + s;
-  setTimeout(startTimer, 1000);
-  
-  // when the timer stops
-  if(s==0){
-    console.log('TEMPO SCADUTO');
-  }
-}
-
-function checkSecond(sec) {
-  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
-  if (sec < 0) {sec = "59"};
-  return sec;
-}
-
 // grafico
 var data = {
   labels: checker,
   datasets: [{
     label: "Andamento emozioni",
     borderColor: "rgba(255,255,255,1)",
-    borderWidth: 3,
+    borderWidth: 5,
     data: emozioniGrafico,
     stepped: true,
   }]
@@ -99,12 +45,15 @@ var graficoEmo = new Chart('chart', {
   data: data
 });
 
+var cont = document.getElementById("container")
+
+
 var veta;
 var vEmozione;
 
 const config = {
   initialWaitMs: 0,
-  periodMs: 2500
+  periodMs: 1500
 };
 
 
@@ -146,6 +95,7 @@ window.addEventListener(CY.modules().DATA_AGGREGATOR.eventName, (evt) => {
     evt.detail.emotion_Happy.avg
   ]
 
+
   const max = Math.max.apply(null, emozioni);
 
   index = emozioni.indexOf(max);
@@ -184,10 +134,13 @@ window.addEventListener(CY.modules().DATA_AGGREGATOR.eventName, (evt) => {
 
   console.log(storicoEmozioni, varpiufreq)
 
+  $(".square").css("width", 800/checker + "px")
+
   if (varpiufreq == 0) {
     document.getElementById("emozioneMedia").innerHTML = "Rabbia";
 
-    if (videoisfinito) {adjArrabbiato();} //--> APPENA IL VIDEO FINISCE MI PARTE LA VARIABILE CHE SCATENA VARPIUFREQ
+    // if (videoisfinito) {adjArrabbiato();} //--> APPENA IL VIDEO FINISCE MI PARTE LA VARIABILE CHE SCATENA VARPIUFREQ
+    adjArrabbiato();
 
   } else if (varpiufreq == 1) {
     document.getElementById("emozioneMedia").innerHTML = "Disgusto"
@@ -195,14 +148,9 @@ window.addEventListener(CY.modules().DATA_AGGREGATOR.eventName, (evt) => {
     document.getElementById("emozioneMedia").innerHTML = "Paura"
   } else if (varpiufreq == 6) {
     document.getElementById("emozioneMedia").innerHTML = "Felicità"
-    linkAudio = 'audio.mp3';
-    //audioObj = new Audio(linkAudio).play();
-
+    continuaFelice()
   } else if (varpiufreq == 4) {
     document.getElementById("emozioneMedia").innerHTML = "Neutrale"
-    linkAudio = 'triste.m4a';
-    //audioObj = new Audio(linkAudio).play();
-
   } else if (varpiufreq == 3) {
     document.getElementById("emozioneMedia").innerHTML = "Triste"
   } else if (varpiufreq == 5) {
@@ -226,6 +174,7 @@ window.addEventListener(CY.modules().DATA_AGGREGATOR.eventName, (evt) => {
   graficoEmo.data.datasets[0].data = asseX;
   graficoEmo.data.labels = asseY;
   graficoEmo.update();
+
 
   if (index == 0) {
     emozioneMax = "Rabbia"
@@ -263,101 +212,146 @@ async function adjArrabbiato() {
   }
 }
 
+async function continuaFelice() {
+  if (isConnectted) {
+    await writer.write(enc.encode(`255-0-255@`)); //warm color
+    return;
+  }
+}
+
+
 async function felice() {
-  document.getElementById('emotion').innerHTML = "<b>NOW: FELICE</b>";
+  document.getElementById('emotion').innerHTML = "<b>Emotion: FELICE</b>";
   // $("#colore").css("background", "green")
 
   r.style.setProperty('--color-1', 'rgba(0,255,0,1)');
   r.style.setProperty('--color-2', 'rgba(0,255,0,0)');
 
-  if (isConnectted) {
-    await writer.write(enc.encode(`0-255-0@`));
-    return;
-  }
+  var div = document.createElement("div");
+  div.setAttribute("class", "square");
+  div.setAttribute("id", "felice");
+  cont.appendChild(div);
+
+
+  // if (isConnectted) {
+  //   await writer.write(enc.encode(`0-255-0@`));
+  //   return;
+  // }
   return;
 }
 
 
 async function rabbia() {
-  document.getElementById('emotion').innerHTML = "<b>NOW: ARRABBIATO</b>";
+  document.getElementById('emotion').innerHTML = "<b>Emotion: ARRABBIATO</b>";
   // $("#colore").css("background", "red")
 
   r.style.setProperty('--color-1', 'rgba(255,0,0,1)');
   r.style.setProperty('--color-2', 'rgba(255,0,0,0)');
 
-  if (isConnectted) {
-    await writer.write(enc.encode(`255-0-0@`));
-    return;
-  }
+  var div = document.createElement("div");
+  div.setAttribute("class", "square");
+  div.setAttribute("id", "rabbia");
+  cont.appendChild(div);
+
+  // if (isConnectted) {
+  //   await writer.write(enc.encode(`255-0-0@`));
+  //   return;
+  // }
   return;
 }
 
 async function triste() {
-  document.getElementById('emotion').innerHTML = "<b>NOW: TRISTE</b>";
+  document.getElementById('emotion').innerHTML = "<b>Emotion: TRISTE</b>";
   // $("#colore").css("background", "blue")
+
+  var div = document.createElement("div");
+  div.setAttribute("class", "square");
+  div.setAttribute("id", "triste");
+  cont.appendChild(div);
+
 
   r.style.setProperty('--color-1', 'rgba(0,0,255,1)');
   r.style.setProperty('--color-2', 'rgba(0,0,255,0)');
 
-  if (isConnectted) {
-    await writer.write(enc.encode(`0-0-255@`));
-    return;
-  }
+  // if (isConnectted) {
+  //   await writer.write(enc.encode(`0-0-255@`));
+  //   return;
+  // }
   return;
 }
 
 async function disgusto() {
-  document.getElementById('emotion').innerHTML = "<b>NOW: DISGUSTATO</b>";
+  document.getElementById('emotion').innerHTML = "<b>Emotion: DISGUSTATO</b>";
   // $("#colore").css("background", "yellow")
+
+  var div = document.createElement("div");
+  div.setAttribute("class", "square");
+  div.setAttribute("id", "disgusto");
+  cont.appendChild(div);
 
   r.style.setProperty('--color-1', 'rgba(255,255,0,1)');
   r.style.setProperty('--color-2', 'rgba(255,255,0,0)');
 
-  if (isConnectted) {
-    await writer.write(enc.encode(`255-255-0@`));
-    return;
-  }
+  // if (isConnectted) {
+  //   await writer.write(enc.encode(`255-255-0@`));
+  //   return;
+  // }
   return;
 }
 
 async function neutrale() {
-  document.getElementById('emotion').innerHTML = "<b>NOW: NEUTRALE</b>";
+  document.getElementById('emotion').innerHTML = "<b>Emotion: NEUTRALE</b>";
   // $("#colore").css("background", "white")
+
+  var div = document.createElement("div");
+  div.setAttribute("class", "square");
+  div.setAttribute("id", "neutrale");
+  cont.appendChild(div);
 
   r.style.setProperty('--color-1', 'rgba(255,255,255,1)');
   r.style.setProperty('--color-2', 'rgba(255,255,255,0)');
 
-  if (isConnectted) {
-    await writer.write(enc.encode(`255-255-255@`));
-    return;
-  }
+  // if (isConnectted) {
+  //   await writer.write(enc.encode(`255-255-255@`));
+  //   return;
+  // }
   return;
 }
 
 async function paura() {
-  document.getElementById('emotion').innerHTML = "<b>NOW: IMPAURITO</b>";
+  document.getElementById('emotion').innerHTML = "<b>Emotion: IMPAURITO</b>";
   // $("#colore").css("background", "magenta")
+
+  var div = document.createElement("div");
+  div.setAttribute("class", "square");
+  div.setAttribute("id", "paura");
+  cont.appendChild(div);
 
   r.style.setProperty('--color-1', 'rgba(255,0,255,1)');
   r.style.setProperty('--color-2', 'rgba(255,0,255,0)');
 
-  if (isConnectted) {
-    await writer.write(enc.encode(`255-0-255@`));
-    return;
-  }
+  // if (isConnectted) {
+  //   await writer.write(enc.encode(`255-0-255@`));
+  //   return;
+  // }
   return;
 }
 
 async function sorpreso() {
-  document.getElementById('emotion').innerHTML = "<b>NOW: SORPRESO</b>";
+  document.getElementById('emotion').innerHTML = "<b>Emotion: SORPRESO</b>";
   // $("#colore").css("background", "cyan")
+
+  var div = document.createElement("div");
+  div.setAttribute("class", "square");
+  div.setAttribute("id", "sorpreso");
+  cont.appendChild(div);
 
   r.style.setProperty('--color-1', 'rgba(0,255,255,1)');
   r.style.setProperty('--color-2', 'rgba(0,255,255,0)');
 
-  if (isConnectted) {
-    await writer.write(enc.encode(`0-255-255@`));
-    return;
-  }
+  // if (isConnectted) {
+  //   await writer.write(enc.encode(`0-255-255@`));
+  //   return;
+  // }
   return;
 }
